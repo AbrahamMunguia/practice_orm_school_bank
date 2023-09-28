f9d7142a0a0daa45176bacac607601f5ed6b724b
cesar munguia <c.munguiabrito@gmail.com>
2023-09-28 00:29:41 +0000

    feat: router

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Santo batman")
+const express = require("express")
+
+const app = express()
+const PORT = 3000
+const routerAPI = require('./routes/index')
+app.get('/',(req,res)=>{
+  res.send("Hello world!")
+})
+routerAPI(app)
+app.listen(PORT,()=>{
+
+})
+
